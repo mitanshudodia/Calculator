1b828ea537efd42b27836d5994b02d2e89a6f551
Mitanshu Dodia <mitanshudodia2002@gmail.com>
2020-12-01 21:57:39 +0530

    Second Commit

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -2,15 +2,35 @@ package com.example.mycalculator
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    var lastNumeric = false
+    var lastDecimal = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
     fun onDigit(view:View){
-        Toast.makeText(this,"Button Works",Toast.LENGTH_SHORT).show()
+        tvInput.append((view as Button).text)
+        lastNumeric = true
+    }
+    fun onClear(view: View){
+        tvInput.text = ""
+        lastNumeric = false
+        lastDecimal = false
+    }
+    fun onDecimalPoint(view: View){
+        if(lastNumeric && !lastDecimal){
+            tvInput.append(".")
+            lastNumeric = false
+            lastDecimal = true
+        }
+
     }
 }
